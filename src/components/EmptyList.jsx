fb02ee847577e25407980f98059255c57c64d55e
Cesar Mejias <cesarfar376@gmail.com>
2022-06-26 21:13:29 -0400

    [UPDATE] Added 12px horizontal padding to the EmptyList component

--- src/components/EmptyList.jsx
+++ src/components/EmptyList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function EmptyList() {
   return (
-    <section className='center-flex bg-skin-todos mb-4 py-16 text-skin-text'>
+    <section className='center-flex bg-skin-todos mb-4 py-16 px-3 text-skin-text'>
       You don't have todos, create now!!
     </section>
   );
